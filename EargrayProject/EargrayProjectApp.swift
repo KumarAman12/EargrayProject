84c0888eb4630f02c80a67fe11a66b863eed4212
KumarAman12 <aman.kumar@hingehealth.com>
2024-01-27 12:54:36 +0530

    Initial Commit

---
+++ EargrayProject/EargrayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EargrayProjectApp.swift
+//  EargrayProject
+//
+//  Created by Aman Kumar on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct EargrayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
